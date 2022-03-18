c4d037d93c1a2df689806f4b76440e560ad057bb
ewa-j <e.jablonska@elsevier.com>
2022-03-18 15:29:45 +0000

    added method call to controller. Ewa's turn to write a test;)

--- src/main/java/school/controller/StudentController.java
+++ src/main/java/school/controller/StudentController.java
@@ -1,15 +1,22 @@
 package school.controller;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 import school.model.Student;
+import school.service.StudentService;
 
 @RestController
 public class StudentController {
 
-  @PostMapping("/student")
-  public void createStudent(Student student) {
+  @Autowired
+  StudentService studentService;
 
+  @PostMapping("/student")
+  public void createStudent(@RequestBody Student student) {
+    studentService.createStudent(student);
   }
 
 
--- src/test/java/school/controller/StudentControllerTest.java
+++ src/test/java/school/controller/StudentControllerTest.java
@@ -1,14 +1,19 @@
 package school.controller;
 
+import static org.mockito.MockitoAnnotations.openMocks;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
+
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.mockito.InjectMocks;
 import org.mockito.Mock;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.http.MediaType;
 import org.springframework.test.web.servlet.MockMvc;
 import school.model.Student;
 import school.service.StudentService;
@@ -22,11 +27,16 @@ class StudentControllerTest {
   @Autowired
   private MockMvc mockMvc;
 
-  @Mock
+  @MockBean
   StudentService studentService;
 
   @InjectMocks
   private StudentController studentController;
+
+  @BeforeEach
+  public void setUp() {
+    openMocks(this);
+  }
 
   @Test
   public void shouldReturnOk() throws Exception {
@@ -35,9 +45,11 @@ class StudentControllerTest {
   }
 
   @Test
-  public void shouldSendAStudentToTheStudentService() {
+  public void shouldSendAStudentToTheStudentService() throws Exception {
     Student student = new Student();
     studentController.createStudent(student);
+//    mockMvc.perform(post("/student").contentType(MediaType.APPLICATION_JSON).content("{}"))
+//        .andExpect(status().isOk());
     verify(studentService).createStudent(student);
 
   }
